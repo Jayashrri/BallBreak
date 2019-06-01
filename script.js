4f1951ba5d05c073fe2200f925ef239708583dd7
jayashrri <balaji.jayashrri@gmail.com>
2019-06-01 16:42:01 +0530

    Rock Class

--- script.js
+++ script.js
@@ -1,47 +1,66 @@
+const startbtn=document.getElementById('start');
+
 var ctx;
 
 var minrockH=350;
-var rockH;
-var x;
-var y;
-var dx=1;
-var dy=0;
-var lr=Math.round(Math.random());
-if(lr==1)
-    x=410-20;
-else
-    x=20;
-rockH=Math.floor(Math.random()*(530-minrockH)+minrockH);
-y=rockH;
+var maxStr=10;
+
+
+class Rock {
+    constructor(){
+        this.radius=20;
+        if(Math.round(Math.random())==1)
+            this.x=390;
+        else this.x=20;
+        this.rockH=Math.floor(Math.random()*(530-minrockH)+minrockH);
+        this.dx=1;
+        this.dy=0;
+        this.y=this.rockH;
+        this.strength=Math.floor(Math.random()*10+1);
+    }
+
+    drawrock(){
+        ctx.clearRect(0,100,410,450);
+        ctx.fillStyle="#396639";
+        ctx.fillRect(0,0,410,100);
+        ctx.fillStyle="#c5dfe0";
+        ctx.fillRect(0,100,410,450);
+        ctx.beginPath();
+        ctx.fillStyle="#751525";
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,true);
+        ctx.closePath();
+        ctx.fill();
+    }
+
+    updaterock(){
+        if(this.x<20||this.x>390){
+            this.dx=-this.dx;
+        }
+        if(this.y<120){
+            this.dy=-this.dy;
+            this.y=120;
+        }
+        if(this.y>this.rockH)
+            this.dy=0;
+        this.dy+=9.8/200;
+        this.y-=this.dy;
+        this.x+=this.dx;
+    }
+}
+
 
 function init(){
+    startbtn.style.display="none";
     var gamescreen = document.getElementById('gamescreen');
     ctx = gamescreen.getContext('2d');
     ctx.transform(1,0,0,-1,0,550);
-    setInterval(rockbounce,10);
+    let ball = new Rock();
+    setInterval(function(){
+        ball.drawrock();
+        ball.updaterock();
+    },10);
 }
 
 
-function rockbounce(){
-    ctx.clearRect(0,100,410,450);
-    ctx.fillStyle="#396639";
-    ctx.fillRect(0,0,410,100);
-    ctx.fillStyle="#c5dfe0";
-    ctx.fillRect(0,100,410,450);
-    var rock = gamescreen.getContext('2d');
-    rock.beginPath();
-    rock.fillStyle="#751525";
-    rock.arc(x,y,20,0,Math.PI*2,true);
-    rock.closePath();
-    rock.fill();
-    dy+=9.8/200;
-    if(x<0||x>390)
-        dx=-dx;
-    if(y<120)
-        dy=-dy;
-    if(y>rockH){
-        dy=9.8/200;
-    }
-    y-=dy;
-    x+=dx;
-}
+startbtn.style.display="block";
+startbtn.onclick=init;
